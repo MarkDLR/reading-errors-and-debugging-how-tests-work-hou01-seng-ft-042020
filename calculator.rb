61d1e0a55332a922fa8d3b167fb0f3e773caecc2
Mark De La Rosa <mark.p.dlr@gmail.com>
2020-04-01 23:59:14 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,5 +2,7 @@
 first_number = 3
 second_number = 4
 
-def sum = first_number + second_number
+def sum(first_number, second_number)
+  first_number + second_number
+end
 difference = first number - second_number
